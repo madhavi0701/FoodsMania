857c3aa49d45826799e64d3ca29ee27e23132d5f
madhavi0701 <gaikwadmadhavi07@gamil.com>
2020-12-26 23:05:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/foodsmania/PrefConstant.java
@@ -0,0 +1,7 @@
+package com.example.foodsmania;
+
+public class PrefConstant {
+    public static String SHARED_PREFERENCE_NAME = "food_app_pref";
+    public static String IS_LOGGED_IN = "is_logged_in";
+   public static String ON_BOARDED_SUCCESSFULLY = "on_boarded_successfully";
+}
---
+++ app/src/main/java/com/example/foodsmania/activity/SplashActivity.java
@@ -0,0 +1,72 @@
+package com.example.foodsmania.activity;
+
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodsmania.PrefConstant;
+import com.example.foodsmania.R;
+import com.example.foodsmania.onboarding.OnBoardingActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    Animation anim;
+    ImageView imageView;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView = (ImageView)findViewById(R.id.imageView6_d);
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                setUpSharedPreferences();
+                checkLoginStatus();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imageView.startAnimation(anim);
+    }
+
+    private void setUpSharedPreferences() {
+        sharedPreferences = getSharedPreferences(PrefConstant.SHARED_PREFERENCE_NAME,MODE_PRIVATE);
+    }
+
+    private void checkLoginStatus() {
+        boolean isLoggedIn = sharedPreferences.getBoolean(PrefConstant.IS_LOGGED_IN,false);
+        boolean isBoardingSuccess = sharedPreferences.getBoolean(PrefConstant.ON_BOARDED_SUCCESSFULLY,false);
+
+        if (isLoggedIn){
+            Intent intent = new Intent(SplashActivity.this, HomePage.class);
+            startActivity(intent);
+        }
+        else{
+            if (isBoardingSuccess) {
+                Intent intent =new Intent(this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                Intent intent =new Intent(this, OnBoardingActivity.class);
+                startActivity(intent);
+            }
+        }
+        finish();
+    }
+
+}
